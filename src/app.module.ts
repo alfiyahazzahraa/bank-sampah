c8630c771f54c2297cdcbd8e674f7ca691ba7785
Alfiyah fadantya zahra <alfiyahfadantyaazzahra@email.com>
2026-09-21 09:06:06 +0700

    chore: update app module, main.ts, dan setup folder uploads

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AppMakerModule } from './app-maker/app-maker.module';
+import { AuthModule } from './auth/auth.module';
+import { KategoriSampahModule } from './kategori-sampah/kategori-sampah.module';
+import { HadiahModule } from './hadiah/hadiah.module';
+import { SetorSampahModule } from './setor-sampah/setor-sampah.module';
+import { NasabahModule } from './nasabah/nasabah.module';
+import { PenukaranPoinModule } from './penukaran-poin/penukaran-poin.module';
+import { SeedModule } from './seed/seed.module';
+import { RekapitulasiModule } from './rekapitulasi/rekapitulasi.module';
+import { DashboardService } from './dashboard/dashboard.service';
+import { DashboardController } from './dashboard/dashboard.controller';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, AppMakerModule, AuthModule, KategoriSampahModule, HadiahModule, SetorSampahModule, NasabahModule, PenukaranPoinModule, SeedModule, RekapitulasiModule, DashboardModule],
+  controllers: [AppController, DashboardController],
+  providers: [AppService, DashboardService],
 })
 export class AppModule {}
